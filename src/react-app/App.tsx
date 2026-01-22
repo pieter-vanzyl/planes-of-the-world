07797b11bf689dc2ffff847c77ccb0ea66211c34
Pieter van Zyl <pietervanzyl@hotmail.com>
2026-01-22 17:15:11 +0000

    4th

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -10,28 +10,36 @@ import "./App.css";
 function App() {
 
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React + Hono + Cloudflare</h1>
+		<main className="bg-[url(./assets/BackGround.svg)] bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+			<div className="pattern"/>
+			<div className="wrapper">
+				<header>
+					<img src="./assets/file.svg" alt="Plane Banner"/>
+					<h1 className="text-4xl font-bold text-stone-50">Find <span className="text-blue-800">Planes</span> you like</h1>
+				</header>
 
-		</>
+				<p>Search</p>
+			</div>
+
+
+			<div className="items-center justify-center rounded-md p-2 bg-gray-600 shadow-lg">
+				<div className="inline-flex">
+					<a href="https://vite.dev" target="_blank">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+					<a href="https://react.dev" target="_blank">
+						<img src={reactLogo} className="logo react" alt="React logo" />
+					</a>
+					<a href="https://hono.dev/" target="_blank">
+						<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
+					</a>
+					<a href="https://workers.cloudflare.com/" target="_blank">
+						<img src={cloudflareLogo} className="logo cloudflare" alt="Cloudflare logo"/>
+					</a>
+				</div>
+				<h2 className="text-stone-50">Vite + React + Hono + Cloudflare</h2>
+			</div>
+		</main>
 	);
 }
 
