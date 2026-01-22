f054852cc50237294688b4466f28bb4d3044db73
Pieter van Zyl <pietervanzyl@hotmail.com>
2026-01-22 15:15:19 +0000

    2nd

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -32,33 +32,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React + Hono + Cloudflare</h1>
-			<div className="card">
-				<button
-					onClick={() => setCount((count) => count + 1)}
-					aria-label="increment"
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<div className="card">
-				<button
-					onClick={() => {
-						fetch("/api/")
-							.then((res) => res.json() as Promise<{ name: string }>)
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>worker/index.ts</code> to change the name
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
+
 		</>
 	);
 }
