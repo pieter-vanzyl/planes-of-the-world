80d70e9ede1336138c2c92cf27e81b932afdf76d
Pieter van Zyl <pietervanzyl@hotmail.com>
2026-01-22 15:17:03 +0000

    3rd

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import { useState } from "react";
+//import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
@@ -8,8 +8,6 @@ import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const [name, setName] = useState("unknown");
 
 	return (
 		<>
